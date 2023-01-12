0e4ef0cbefd1aedafd28da10ba115a50b38de266
[allanprado] <[allanprado.it@gmail.com]>
2023-01-12 14:25:03 -0300

    I will continue the second chapter tomorrow

--- hfad2/app/src/main/java/com/hfad/beeradviser/MainActivity.kt
+++ hfad2/app/src/main/java/com/hfad/beeradviser/MainActivity.kt
@@ -2,10 +2,15 @@ package com.hfad.beeradviser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //val findBeer = findViewById<Button>(R.id.find_beer)
+        //findBeer.setOnClickListener {   }
+        //This part is the next part os the chapter 2, I will finish them tomorrow!
     }
 }
